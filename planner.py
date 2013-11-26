82387900704dfbe673670d5c7db1d8c951b0e4da
dloti <dloti@aigb-PC.(none)>
2013-11-26 20:19:46 +0100

    py validation

--- planner.py
+++ planner.py
@@ -7,6 +7,15 @@ instances_num = int(sys.argv[2])
 for i in range(1,instances_num+1):
 	subprocess.call(["./planner", str(sys.argv[1]), str(i)])
 
-
+shutil.copy2('domain.txt', '../PlanValidator/domain.txt')
+shutil.copy2('plans.txt', '../PlanValidator/plans.txt')
 shutil.copy2('plans.txt', '../Student/plans.txt')
 
+print "--- Training set validation --"
+for i in range(1,instances_num+1):
+	child = subprocess.call(["../PlanValidator/validator", str(sys.argv[1]), str(i)])
+	if child != 0:
+		print "ERR PLANS not valid!"
+		break
+		
+print "Training set valid"
